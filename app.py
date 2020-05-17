998ed12dacb4aac0933a8e46a86c65c12297be53
datvithanh98 <datvithanh98@gmail.com>
2020-05-17 17:03:39 +0700

    init

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask_restful import reqparse, Resource, Api
+from infer import infer
+
+app = Flask(__name__)
+api = Api(app)
+
+parser = reqparse.RequestParser()
+parser.add_argument('image')
+
+class Predict(Resource):
+    def post(self):
+        args = parser.parse_args()
+
+        try: 
+            output = infer(args['image'])
+            return {"name": output}
+        except:
+            return {"message": "something wrong happened, perhaps wrong base64 format"}
+
+api.add_resource(Predict, '/predict')
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ infer.py
@@ -0,0 +1,34 @@
+from PIL import Image
+import cv2
+import numpy as np
+import tensorflow as tf
+import base64
+import io
+
+CLASS_NAMES = ['xoi', 'roll', 'noodles', 'pizza', 'milktea', 'caramen', 'banh_mi', 'birthcake', 'che', 'banh_cuon']
+
+interpreter = tf.lite.Interpreter(model_path="model.tflite")
+interpreter.allocate_tensors()
+
+def stringToRGB(base64_string):
+    imgdata = base64.b64decode(base64_string)
+    image = Image.open(io.BytesIO(imgdata))
+    return cv2.cvtColor(np.array(image), cv2.COLOR_BGR2RGB)
+
+def preprocess_image(image):
+    image = image/255
+    resized_img = cv2.resize(image, dsize=(224, 224), interpolation=cv2.INTER_CUBIC)
+    batch_inp = np.expand_dims(resized_img, axis=0)
+    return batch_inp
+
+def infer(base64):
+    im = stringToRGB(base64)
+    inp = preprocess_image(im).astype(np.float32)
+
+    interpreter.set_tensor(0, inp)
+    interpreter.invoke()
+
+    output_data = interpreter.get_tensor(178)
+    return CLASS_NAMES[np.argmax(output_data[0])]
+
+    
